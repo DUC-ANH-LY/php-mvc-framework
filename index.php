21629d4cb0b48204dd726dcc80f7ae0ce7d27ca4
ducanhly <namobilegame@gmail.com>
2024-07-24 23:48:13 +0700

    add model

--- index.php
+++ index.php
@@ -1,35 +1,28 @@
 <?php
 
-$dsn = "mysql:host=localhost;dbname=product_db;charset=utf8;port=3306";
+require "model.php";
 
-$pdo = new PDO($dsn, "product_db_user", "secret", [
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-]);
+$model = new Model;
 
-$stmt = $pdo->query("SELECT * FROM product");
-
-$products = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$products = $model->getData();
 
 ?>
 <!DOCTYPE html>
 <html>
-
 <head>
     <title>Products</title>
     <meta charset="UTF-8">
 </head>
-
 <body>
 
-    <h1>Products</h1>
+<h1>Products</h1>
 
-    <?php foreach ($products as $product) : ?>
+<?php foreach ($products as $product): ?>
 
-        <h2><?= htmlspecialchars($product["name"]) ?></h2>
-        <p><?= htmlspecialchars($product["description"]) ?></p>
+    <h2><?= htmlspecialchars($product["name"]) ?></h2>
+    <p><?= htmlspecialchars($product["description"]) ?></p>
 
-    <?php endforeach; ?>
+<?php endforeach; ?>
 
 </body>
-
 </html>
