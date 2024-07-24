8964c98286b725467bae5d55f504bebf803f6fc0
ducanhly <namobilegame@gmail.com>
2024-07-25 00:20:07 +0700

    add pretty url

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
+exit("Hello from index.php");
+
 $action = $_GET["action"];
 $controller = $_GET["controller"];
 
@@ -8,7 +10,3 @@ require "src/controllers/$controller.php";
 $controller_object = new $controller;
 
 $controller_object->$action();
-
-// /index.php?controller=products&action=index
-// /index.php?controller=products&action=show
-// /index.php?controller=home&action=index
